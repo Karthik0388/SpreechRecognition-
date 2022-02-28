f6f5e317e8fdaa764f3a998601987641936c8c42
Karthik0388 <94730148+Karthik0388@users.noreply.github.com>
2022-02-28 13:09:08 +0530

    this is first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+window.SpeechRecognition = window.webkitSpeechRecognition;
+let recognition = new SpeechRecognition();
+
+let p = document.createElement('p');
+let template = document.getElementById('template');
+
+template.appendChild(p);
+let value = 1;
+
+recognition.addEventListener("result", e => {
+    let text = (e.results[0][0].transcript);
+    p.innerHTML = text;
+    if (e.result[0].isFinal === true) {
+        p = document.createElement('p');
+        template.appendChild(p);
+    }
+    window.localStorage.setItem(`key:${value}`, text);
+    value++;
+});
+
+recognition.addEventListener('end', recognition.start);
+recognition.start();
